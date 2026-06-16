e8cf7429f8f267d086e46ef1587132e6936deca2
Jose Lima <joselimaprofissional18@gmail.com>
2026-06-16 00:40:05 -0300

    Banco configurado

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,17 +2,22 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = pool;
-
-pool.getConnection()
+pool
+  .getConnection()
   .then(() => {
     console.log("Banco conectado!");
   })
   .catch((err) => {
     console.error("Erro ao conectar:", err.message);
   });
+
+module.exports = pool;
